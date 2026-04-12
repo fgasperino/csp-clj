78903015123033cc49af7c2ec66a5b05f2f90021
fgasperino <franco.gasperino@gmail.com>
2026-04-12 01:01:48 -0700

    * Preparing for 0.0.1 deploy

--- build.clj
+++ build.clj
@@ -3,7 +3,7 @@
   (:require [clojure.tools.build.api :as b]))
 
 (def lib 'org.clojars.fgasperino/csp-clj)
-(def version "0.0.0")
+(def version "0.0.1")
 (def class-dir "target/classes")
 (def basis (b/create-basis {:project "deps.edn"}))
 (def jar-file (format "target/%s-%s.jar" (name lib) version))
